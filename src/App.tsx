28007464ca526e77a0d67a476c1cb5c9fd3c669d
GermanGambarte <germangambarte@gmail.com>
2024-03-21 08:40:00 -0300

    add: random color generator project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [currentColor, setCurrentColor] = useState<string>('')
+
+  function generateColorValue() {
+    return Math.floor(Math.random() * 255 + 1)
+  }
+
+  function generateHEXColor() {
+    let color: string = '#'
+    for (let i = 0; i < 3; i++) {
+      color += generateColorValue().toString(16).padStart(2, '0')
+    }
+    setCurrentColor(color)
+  }
+
+  function generateRGBColor() {
+    const color: number[] = []
+    for (let i = 0; i < 3; i++) {
+      color.push(generateColorValue())
+    }
+    const [r, g, b] = color
+    setCurrentColor(`rgb(${r},${g},${b})`)
+  }
+
+  useEffect(() => {
+    generateHEXColor()
+  }, [])
+
   return (
     <>
-      <h1>Holisss👋🏼</h1>
+      <header>
+        <button onClick={generateHEXColor}>Create HEX Color</button>
+        <button onClick={generateRGBColor}>Create RGB Color</button>
+        <button onClick={generateHEXColor}>Generate Random Color</button>
+      </header>
+      <main style={{ backgroundColor: `${currentColor}` }}>
+        <h1>{currentColor}</h1>
+      </main>
     </>
   )
 }
