27db6099181e201a6b53acae57c17985212a52f1
GermanGambarte <germangambarte@gmail.com>
2024-03-23 14:26:31 -0300

    add star rating project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react'
 import './App.css'
+import { HiOutlineStar } from 'react-icons/hi2'
 
 function App() {
+  const noOfStars = Array(5).fill(null)
+  const [rating, setRating] = useState<number>(0)
+  const [hover, setHover] = useState<number>(0)
+
+  function handleClick(currentIdx: number) {
+    setRating(currentIdx)
+  }
+
+  function handleMouseEnter(currentIdx: number) {
+    setHover(currentIdx)
+  }
+
+  function handleMouseLeave() {
+    setHover(rating)
+  }
+
   return (
-    <>
-      <h1>Holisss👋🏼</h1>
-    </>
+    <main>
+      <h1>Star Rating</h1>
+      <section>
+        {
+          noOfStars.map((_, index) => {
+            return <HiOutlineStar
+              key={index}
+              className={index <= (hover || rating) ? 'active' : 'no-active'}
+              onClick={() => handleClick(index)}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            />
+
+          })
+        }
+      </section>
+    </main>
   )
 }
 
